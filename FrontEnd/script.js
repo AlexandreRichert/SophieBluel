3282b5db754f7decda34672f6ef88278d70478a7
Alexandre Richert <richert.alexandre31@gmail.com>
2023-02-08 16:43:37 +0100

    Fetch visible sur la console de l'inspecteur

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,2 +1,9 @@
-const r = fetch ('http://localhost:5678/api/works');
-console.log(r);
+const API = 'http://localhost:5678/api/works'
+
+async function getAPI() {
+    const response = await fetch(API);
+    const json= await response.json();
+    console.log(json);
+}
+
+getAPI();
