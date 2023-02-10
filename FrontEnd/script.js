9bba7b607f9a0ea7f9a400282337d19ee8678516
Alexandre Richert <richert.alexandre31@gmail.com>
2023-02-10 16:10:24 +0100

    remplacement des données HTML par les travaux de l'api (dynamiquement)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,35 @@
-const API = 'http://localhost:5678/api/works'
 
-async function getAPI() {
-    const response = await fetch(API);
-    const json= await response.json();
-    console.log(json);
-}
 
-getAPI();
+/// Récupération de tous les travaux de l'API ///
+
+fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(projects => {
+
+
+        const gallery = document.querySelector('.gallery');
+
+        /// suppression du contenu déjà existant dans la div gallery ///
+        gallery.innerHTML = "" ;
+
+
+        /// création des éléments de gallery pour chaque travaux /// 
+        projects.forEach(element => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const figcaption = document.createElement('figcaption');
+
+            /// attribution à chaque élément, l'image et le titre ///
+            img.src = element.imageUrl;
+            img.alt = element.title;
+            figcaption.textContent = element.title;
+
+            /// attribution de l'image et de figcaption à son élément parent : figure ///
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            /// attribution de figure à son élément parent : gallery ///
+            gallery.appendChild(figure);
+            
+        });
+    })
+    .catch((erreur) => console.log('Erreur : ' + erreur));
